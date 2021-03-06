76fb7c593f021a3c218f866b426a4d454cab107d
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-03-06 17:34:07 -0400

    Adding a head to my html

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link"
+import Head from "next/head"
 
 
 export default function FistPost () {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            
             <h1>
                 First Post
             </h1>
